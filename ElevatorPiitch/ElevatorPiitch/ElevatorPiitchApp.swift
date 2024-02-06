6e284acd0350fbd57bb3bd705d2adf44362c5564
cmunoz602 <102495225+cmunoz602@users.noreply.github.com>
2024-02-06 15:51:58 -0700

    Add files via upload

---
+++ ElevatorPiitch/ElevatorPiitch/ElevatorPiitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ElevatorPiitchApp.swift
+//  ElevatorPiitch
+//
+//  Created by Cristian Munoz on 2/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ElevatorPiitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
